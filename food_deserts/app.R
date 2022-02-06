0a772fc55197ee51514995ee8dfdab448f8bbb45
kieramatiska <kmatiska@ucsb.edu>
2022-02-06 09:06:36 -0800

    Added another widget

--- food_deserts/app.R
+++ food_deserts/app.R
@@ -59,7 +59,31 @@ ui <- fluidPage(
                   ), # end sidebarPanel
                   mainPanel(
                     print(incomeRange) # need something for output
-                  )
+                  ), # end main panel
+                  sidebarPanel(
+                    selectInput("select", label = h3("Select State:"), 
+                                choices = list("Alabama" = "Alabama", "Alaska" = "Alaska", "Arizona" = "Arizona",
+                                               "Arkansas" = "Arkansas", "California" = "California", "Colorado" = "Colorado",
+                                               "Connecticut" = "Connecticut", "Delaware" = "Delaware", "Florida" = "Florida",
+                                               "Georgia" = "Georgia", "Hawaii" = "Hawaii", "Idaho" = "Idaho",
+                                               "Illinois" = "Illinois", "Indiana" = "Indiana", "Iowa" = "Iowa",
+                                               "Kansas" = "Kansas", "Kentucky" = "Kentucky", "Louisiana" = "Louisiana",
+                                               "Maine" = "Maine", "Maryland" = "Maryland", "Massechussetts" = "Massechussetts",
+                                               "Michigan" = "Michigan", "Minnesota" = "Minnesota", "Mississippi" = "Mississippi",
+                                               "Missouri" = "Missouri", "Montana" = "Montana", "Nebraska" = "Nebraska",
+                                               "Nevada" = "Nevada", "New Hampshire" = "New Hampshire", "New Jersey" = "New Jersey",
+                                               "New Mexico" = "New Mexico", "New York" = "New York", "North Carolina" = "North Carolina",
+                                               "North Dakota" = "North Dakota", "Ohio" = "Ohio", "Oklahoma" = "Oklahoma",
+                                               "Oregon" = "Oregon", "Pennsylvania" = "Pennsylvania", "Rhode Island" = "Rhode Island",
+                                               "South Carolina" = "South Carolina", "South Dakota" = "South Dakota", "Tennessee" = "Tennessee",
+                                               "Texas" = "Texas", "Utah" = "Utah", "Vermont" = "Vermont",
+                                               "Virginia" = "Virginia", "Washington" = "Washington", "West Virginia" = "West Virginia",
+                                               "Wisconsin" = "Wisconsin", "Wyoming" = "Wyoming"), 
+                                selected = "Alabama"),
+                  ) # end sidebarPanel
+                  mainPanel(
+                    print(state)
+                  ) # end mainPanel
                 ) # end sidebarLayout
               ), # end tabPanel
                ), # End navbarPage
