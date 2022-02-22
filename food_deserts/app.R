99d397480d925c38306f51b59804ba684b6a4ece
devin-ngo <devinpngo@gmail.com>
2022-02-21 17:27:37 -0800

    Updated select input

--- food_deserts/app.R
+++ food_deserts/app.R
@@ -39,25 +39,8 @@ ui <- fluidPage(theme = shinytheme("sandstone"), # Will probably customize own t
                            tabPanel("W1 - Rural/Urban Breakdown",
                                     sidebarLayout(
                                       sidebarPanel("Breakdown of rural and urban areas by state", #end sidebar panel
-                                      selectInput(inputId = "state", label = h3("Select State"),
-                                                  choices = list("Alabama", "Alaska", "Arizona",
-                                                                 "Arkansas", "California", "Colorado",
-                                                                 "Connecticut", "Delaware", "Florida",
-                                                                 "Georgia", "Hawaii", "Idaho",
-                                                                 "Illinois", "Indiana", "Iowa",
-                                                                 "Kansas", "Kentucky", "Louisiana",
-                                                                 "Maine", "Maryland", "Massachusetts",
-                                                                 "Michigan", "Minnesota", "Mississippi",
-                                                                 "Missouri", "Montana", "Nebraska",
-                                                                 "Nevada", "New Hampshire", "New Jersey",
-                                                                 "New Mexico", "New York", "North Carolina",
-                                                                 "North Dakota", "Ohio", "Oklahoma",
-                                                                 "Oregon", "Pennsylvania", "Rhode Island",
-                                                                 "South Carolina", "South Dakota", "Tennessee",
-                                                                 "Texas", "Utah", "Vermont",
-                                                                 "Virginia", "Washington", "West Virginia",
-                                                                 "Wisconsin", "Wyoming"),
-                                                  selected = "Alabama") # end select input
+                                                   selectInput(inputId = "state", label = h3("Select State"),
+                                                               choices = unique(food_access$state), selected = "Alabama") # end select input
                                     ), # end sidebarPanel 1
                                     mainPanel(
                                       tmapOutput("county_map"))
